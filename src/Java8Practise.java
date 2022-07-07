78b003649b63fbb1a9ba3655ec71563a348329cc
Suresh Kaushik <sureshkaushik20@gmail.com>
2022-07-07 15:03:43 +0530

    first commit

---
+++ src/Java8Practise.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Java8Practise {
+
+    public static void main(String[] arg){
+        List<Integer> list= Arrays.asList(10,15,8,49,25,98,32);
+        list.stream().filter(n->n%2==0).forEach(System.out::println);
+    }
+}
